7fea337ce19bc1ab9f9562f3efd070a7eb978d0a
VictoryK-tech <kemelevictory3802@gmail.com>
2024-08-02 19:19:48 +0100

    loading

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import loader from "../assets/loader.json";
 
 const Loader = () => {
   return (
-    <div className="h-8 w-full">
+    <div className=" w-full">
       <iframe src="https://lottie.host/embed/4b9c5793-6fc6-43e2-a8fe-93284ff8ee08/dHnBJJ4F8v.json"></iframe>
     </div>
   );
